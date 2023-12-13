49448d9da36bf2484113271dde78e395d0cc6384
Pratyusha <111060867+adminpratyusha@users.noreply.github.com>
2023-12-13 12:35:45 +0530

    Create startnginx.groovy

---
+++ vars/startnginx.groovy
@@ -0,0 +1,10 @@
+def start(String OUTPUTFILENAME,String SSHCONFIGNAME){
+   sh "tar -xf ${OUTPUTFILENAME} build"
+                        sshPublisher(publishers: [sshPublisherDesc(configName: /, transfers: [
+                                    sshTransfer(
+                                        execCommand: "sudo cp -rf /home/ubuntu/build/* /var/www/html/ && rm -rf /home/ubuntu/build && sudo systemctl restart nginx",
+                                        execTimeout: 120000
+                                    )
+                                ])
+                    ])               
+}
