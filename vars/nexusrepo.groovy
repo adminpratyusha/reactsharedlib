2ba155160ead32b04326e34e0812a37d1abf5b08
Pratyusha <111060867+adminpratyusha@users.noreply.github.com>
2023-12-13 10:58:24 +0530

    Create nexusrepo.groovy

---
+++ vars/nexusrepo.groovy
@@ -0,0 +1,15 @@
+def pushtonexus(String NEXUS_USERNAME,String NEXUS_PASSWORD,String NEXUS_URL,String NEXUS_REPO_ID,String PACKAGE_NAME){
+     // Construct and execute the curl command
+                        def currentVersion = sh(script: 'node -pe "require(\'./package.json\').version"', returnStdout: true).trim()
+                        // def artifactPath = "${PACKAGE_NAME}/${currentVersion}/${PACKAGE_NAME}-${currentVersion}.${env.BUILD_ID}"
+                        def curlCommand = """
+                          curl -v -u ${NEXUS_USERNAME}:${NEXUS_PASSWORD} --upload-file build.tar.gz ${NEXUS_URL}/repository/${NEXUS_REPO_ID}/${PACKAGE_NAME}/${currentVersion}/${PACKAGE_NAME}-${currentVersion}.${env.BUILD_ID}.tar.gz
+                        """
+                        sh curlCommand
+
+                        // Print deployment information
+                        echo "Artifact deployed to Nexus with version ${currentVersion}"
+
+
+  
+}
