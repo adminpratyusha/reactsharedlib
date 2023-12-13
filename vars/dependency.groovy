61f93024e48a5776e06d37190eb3f62df1e9e62d
Pratyusha <111060867+adminpratyusha@users.noreply.github.com>
2023-12-13 10:52:42 +0530

    Create dependency.groovy

---
+++ vars/dependency.groovy
@@ -0,0 +1,9 @@
+def owasp(){
+           dependencyCheck additionalArguments: ''' 
+                    -o './'
+                    -s './'
+                    -f 'ALL' 
+                    --prettyPrint''', odcInstallation: 'OWASP Dependency-Check Vulnerabilities'
+        dependencyCheckPublisher pattern: 'dependency-check-report.xml'
+      
+}
