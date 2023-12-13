a847aae48826b4d7174c8219d5d57079b63285b0
Pratyusha <111060867+adminpratyusha@users.noreply.github.com>
2023-12-13 11:23:48 +0530

    Create dockertask.groovy

---
+++ vars/dockertask.groovy
@@ -0,0 +1,8 @@
+def pushtodocker(String IMAGE_NAME,String DOCKER_CREDENTIALS_ID){
+           def dockerImage = docker.build("${IMAGE_NAME}:${BUILD_ID}")
+
+                
+                    docker.withRegistry('https://registry.hub.docker.com', DOCKER_CREDENTIALS_ID) {
+                        dockerImage.push()
+
+}
